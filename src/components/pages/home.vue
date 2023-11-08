c58f7384ec62f70f1568a167db8b2cbbd20a282a
JakovSijarto <jakov.sijarto2@gmail.com>
2023-11-08 09:28:02 +0100

    a

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <img src="../../assets/home/herosec_main_photo.png" alt="" class="object-cover w-full h-[100vh]">
   <div class="w-full mb-[100em]">
-    <div class="absolute top-[0%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-between gap-[0.5em] mt-[40em] w-[1280px] max-[1340px]:w-full max-[1340px]:px-2 max-[1100px]:flex-col max-[1100px]:top-[25%] max-[1100px]:items-center ">
+    <div class="absolute top-[0%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-between gap-[0.5em] mt-[40em] w-[1280px] max-[1340px]:w-full max-[1340px]:px-2 max-[1100px]:flex-col max-[1100px]:mt-[32em] max-[1100px]:top-[25%] max-[1100px]:items-center ">
       <div class="text-white w-[900px] max-[1100px]:w-full  max-[754px]:text-center max-[754px]:w-[400px] max-[460px]:w-full">
         <p class="text-[60px] font-bold leading-[1.2em] max-[754px]:text-[35px]">Nudimo Vam usluge profesionalnog čišćenja</p>
         <p class="text-[18px] py-8 w-[600px] max-[1100px]:w-full max-[754px]:text-[14px]">PSClean je profesionalan u industriji čišćenja pružanjem vrhunskih usluga čišćenja i pratećih usluga</p>
@@ -10,38 +10,135 @@
     <a href="#" class="font-bold">Naše Usluge</a>
   </div>
       </div>
-      <div class="bg-linear-css w-[400px] h-[800px] relative top-[-10em] max-[1100px]:top-[10em] max-[1100px]:mt-[10em] max-[460px]:w-full">
-        <div class="mt-[100px] flex px-[30px] text-white flex flex-col">
+      <div class="bg-linear-css w-[400px] h-[800px] relative top-[-10em] max-[1100px]:top-[0em] max-[1100px]:w-[80%] max-[1100px]:mt-[10em] max-[1100px]:bg-[#007CFB] max-[1100px]:h-auto max-[460px]:w-full">
+        <div class="mt-[100px] flex px-[30px] text-white flex flex-col ">
           <p class="text-[24px] font-semibold mb-[24px]">Prijavite se</p>
           <form action="">
-            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white" placeholder="Ime*">
-            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white" placeholder="Broj Mobitela*">
-            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white" placeholder="Email*">
-            <select required class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white">
+            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white rounded-[5px]" placeholder="Ime*">
+            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white rounded-[5px]" placeholder="Broj Mobitela*">
+            <input type="text" class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white rounded-[5px]" placeholder="Email*">
+            <select required class="mb-[18px] h-[46px] bg-[#007CFB] border border-[white] py-[6px] px-[22px] text-[15px] w-full text-white rounded-[5px]">
                 <option value="" disabled selected hidden>Izaberi uslugu*</option>
                 <option value="">1</option>
                 <option value="">2</option>
               <option value="">3</option>
             </select>
-            <textarea name="" id="" class="mb-[18px] h-[100px] max-h-[100px] bg-[#007CFB] border border-[white] py-[15px] px-[22px] text-[15px] w-full text-white" placeholder="Poruka"></textarea>
+            <textarea name="" id="" class="mb-[18px] h-[100px] max-h-[100px] bg-[#007CFB] border border-[white] py-[15px] px-[22px] text-[15px] w-full text-white rounded-[5px]" placeholder="Poruka"></textarea>
             <div class="button" id="button-7">
-    <div id="dub-arrow" class="font-bold">ODMAH!</div>
-    <a href="#" class="font-bold">POŠALJI</a>
-  </div>
+              <div id="dub-arrow" class="font-bold">ODMAH!</div>
+              <a href="#" class="font-bold">POŠALJI</a>
+            </div>
           </form>
         </div>
+      </div>
+    </div>
+    <div class="w-full flex justify-center items-center">
+      <div class="flex w-[1280px] justify-center flex-col items-center">
+        <div class="flex items-center justify-center flex-col pb-[25px]">
+          <div class="flex gap-[1em] my-8 flex-row items-center">
+          <img src="../../assets/home/stars-aboutus-home.svg" alt="" class="w-[34px]">
+          <p class="rubik-font text-[18px] font-semibold text-[#3A4268]">Fetaured service</p>
+        </div>
+        <div class="text-[#082680] text-[36px] font-extrabold flex flex-col text-center">
+          We provide the best services
+          <small class="font-normal mb-[14px]">for your help!</small>
+          <div class="text-center text-[16px] leading-[27px] text-[#3A4268] w-[600px] font-light mt-4">Bixol is a design studio founded in London and expanded our services, and become a multinational firm, offering solutions Worldwide.</div>
+        </div>
+        </div>
+        <div class="flex gap-[2em] mt-[3em]">
+          <div class="w-[340px] h-[320px] box-shadow-aboutus rounded-[10px] flex justify-center items-center flex-col text-center">
+            <img src="../../assets/home/house-aboutus-home.svg" alt="" class="cssdottedborder mb-[14px]">
+            <p class="text-[#082680] text-[22px] font-bold pb-[17px] mb-[14px] small-line">House Cleaning</p>
+            <p class="w-[80%] rubik-font text-[#3A4268]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae illum inventore</p>
+          </div>
+          <div class="w-[340px] h-[320px] box-shadow-aboutus rounded-[10px] flex justify-center items-center flex-col text-center">
+            <img src="../../assets/home/plants-aboutus-home.svg" alt="" class="cssdottedborder mb-[14px]">
+            <p class="text-[#082680] text-[22px] font-bold pb-[17px] mb-[14px] small-line">House Cleaning</p>
+            <p class="w-[80%] rubik-font text-[#3A4268]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae illum inventore</p>
+          </div>
+          <div class="w-[340px] h-[320px] box-shadow-aboutus rounded-[10px] flex justify-center items-center flex-col text-center">
+            <img src="../../assets/home/indoor-aboutus-home.svg" alt="" class="cssdottedborder mb-[14px]">
+            <p class="text-[#082680] text-[22px] font-bold pb-[17px] mb-[14px] small-line">House Cleaning</p>
+            <p class="w-[80%] rubik-font text-[#3A4268]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae illum inventore</p>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+    <div class="w-full flex justify-center items-center pt-[95px]">
+      <div class="flex w-[880px] justify-center flex-col items-center">
+        <div class="flex w-full justify-between items-center">
+          <div>
+            <p class="rubik-font text-[18px] font-semibold text-[#3A4268] flex gap-[1em] items-center"> <img src="../../assets/home/stars-aboutus-home.svg" alt="" class="w-[34px]"> Why choose us?</p>
+            <p class="text-[#082680] text-[36px] font-extrabold flex flex-col text-start mt-[15px]">For our best quality <small class="font-normal mb-[14px]">you have chosen us</small></p>
+          </div>
+          <p class="w-[430px] text-[#3A4268] rubik-font">Our agency can only be as strong as our people & because of this, our team have designed game changing products</p>
+        </div>
+        <vue-tabs>
+    <v-tab title="First tab">
+      First tab content
+    </v-tab>
+
+    <v-tab title="Second tab">
+      Second tab content
+    </v-tab>
+
+    <v-tab title="Third tab">
+      Third tab content
+    </v-tab>
+</vue-tabs>
       </div>
     </div>
   </div>
   
 </template>
 
-<script setup>
+<script>
+import {VueTabs, VTab} from 'vue-nav-tabs'
+import 'vue-nav-tabs/themes/vue-tabs.css'
+
+export default{
+  components: {
+    VueTabs,
+    VTab
+  }
+}
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+@media only screen and (max-width: 1100px) {
+  .bg-linear-css{
+  background: #007CFB !important;
+}
+}
+.small-line::before{
+  content: '';
+    width: 28px;
+    height: 3px;
+    background-color: #007cfb;
+    position: absolute;
+    margin-top: 2em;
+    margin-left: 3.2em;
+}
+
+.cssdottedborder{
+  width: 80px;
+  height: 80px;
+  padding: 7px;
+  border-radius: 50%;
+  border: 2px dashed #000E39;
+  box-shadow: 0 0 0 4px #fff;
+}
+.box-shadow-aboutus{
+  -webkit-box-shadow: 0px 1px 23px -1px #007dfb34; 
+box-shadow: 0px 1px 23px -1px #007dfb44;
+}
 ::placeholder{
   color:white;
+}
+.rubik-font{
+    font-family: 'Rubik', sans-serif !important;
 }
 .bg-linear-css{
   background: rgb(255,255,255);
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from '../src/components/App.vue'
 import Router from './router'
+import VueTabs from 'vue-nav-tabs'
+import 'vue-nav-tabs/themes/vue-tabs.css'
 
-createApp(App).use(Router).mount('#app')
+createApp(App).use(Router).use(VueTabs).mount('#app')
